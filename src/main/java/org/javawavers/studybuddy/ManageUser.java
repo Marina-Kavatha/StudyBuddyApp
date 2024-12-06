527f5f22381fb9de91dd3905014eb7e35a259e9b
= <=>
2024-12-07 01:50:40 +0200

    ManageUser

---
+++ src/main/java/org/javawavers/studybuddy/ManageUser.java
@@ -0,0 +1,40 @@
+package org.javawavers.studybuddy;
+
+import java.util.ArrayList;
+
+// Υπεύθυνη για τη διαχείριση χρηστών
+public class ManageUser extends Manage {
+
+    private ArrayList<String> users;
+
+    public ManageUser() {
+        super("User");
+        this.users = new ArrayList<>();
+    }
+
+    // Προσθήκη χρήστη
+    @Override
+    public void add(Object user) {
+        users.add((String) user);
+        System.out.println("Προστέθηκε χρήστης: " + user);
+    }
+
+    // Αφαίρεση χρήστη
+    @Override
+    public void remove(Object user) {
+        if (users.remove((String) user)) {
+            System.out.println("Αφαιρέθηκε χρήστης: " + user);
+        } else {
+            System.out.println("Ο χρήστης δεν βρέθηκε.");
+        }
+    }
+
+    // Εμφάνιση όλων των χρηστών
+    public void displayUsers() {
+        System.out.println("Λίστα χρηστών:");
+        for (String user : users) {
+            System.out.println("- " + user);
+        }
+    }
+}
+
