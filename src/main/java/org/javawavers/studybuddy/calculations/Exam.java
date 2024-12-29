cbdf48709886a9b89fafedcd98e2159077096a79
Domprou Anna-Eleni <t8230035@aueb.gr>
2024-12-29 20:22:50 +0200

    Create Exam.java

---
+++ src/main/java/org/javawavers/studybuddy/calculations/Exam.java
@@ -0,0 +1,30 @@
+
+import java.time.LocalDate;
+
+public class Exam {
+    private LocalDate examDate;
+    private int pages;
+
+    // Constructor
+    public Exam(LocalDate examDate, int pages) {
+        this.examDate = examDate;
+        this.pages = pages;
+    }
+
+    // Getters and setters
+    public LocalDate getExamDate() {
+        return examDate;
+    }
+
+    public void setExamDate(LocalDate examDate) {
+        this.examDate = examDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
